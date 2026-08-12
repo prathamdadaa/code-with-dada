9139d6bc0efae21010627c34bef245f792ed01b0
Pratham Dada <264655831+prathamdadaa@users.noreply.github.com>
2026-08-12 17:04:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.72%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n; // Cast to 64-bit to prevent overflow on -2^31
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+
+        double ans = 1.0;
+        double current_product = x;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans *= current_product;
+            }
+            current_product *= current_product;
+            N /= 2;
+        }
+
+        return ans;
+    }
+};
